886795470e63bf3cd47c958303e1d574a8b98a6f
momoko <momoko.saunders@rigado.com>
2017-02-07 14:22:34 -0800

    Create Project
    Add git ignore
    Add Bluetooth Manager
    Add CentralManager and Peripheral delegate methods

---
+++ HelloBlue/BluetoothManager.swift
@@ -0,0 +1,47 @@
+//
+//  BluetoothManager.swift
+//  HelloBlue
+//
+//  Created by Momoko Saunders on 2/3/17.
+//  Copyright © 2017 Momoko Saunders. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+class BluetoothManager : NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralManager: CBCentralManager?
+    var peripheral: CBPeripheral?
+        
+    // MARK: Central Manager Delegate Methods
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, willRestoreState dict: [String : Any]) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+    }
+    
+    // MARK: Peripheral Delegate Methods
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+     }
+}
+
---
+++ HelloBlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloBlue
+//
+//  Created by Momoko Saunders on 2/3/17.
+//  Copyright © 2017 Momoko Saunders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
